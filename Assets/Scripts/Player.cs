c550aa92042ef4d03f22de38df2d6cf30216928a
guillaume volgyesi <guillaumevolgyesi@gmail.com>
2017-12-02 13:55:26 +0100

    add player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+    float horizontal;
+    float vertical;
+    Vector2 movement;
+    [SerializeField]
+    float speed;
+    Rigidbody2D body;
+    bool eventEnCours = false;
 
-	// Use this for initialization
-	void Start () {
-		
+    public enum EventState
+    {
+        OUT_EVENT_ZONE,
+        ON_EVENT_ZONE,
+        ON_EVENT
+    }
+    EventState state = EventState.OUT_EVENT_ZONE;
+
+	void Start ()
+    {
+        body = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void Update ()
+    {
+        Debug.Log(state);
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        movement = new Vector2(speed * horizontal,
+                               speed * vertical);
+
+        switch(state)
+        {
+            case EventState.OUT_EVENT_ZONE:
+                eventEnCours = false;
+                break;
+
+            case EventState.ON_EVENT_ZONE:
+                if(Input.GetMouseButtonDown(0))
+                {
+                    state = EventState.ON_EVENT;
+                }
+                break;
+
+            case EventState.ON_EVENT:
+                eventEnCours = true;
+                break;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        body.velocity = movement;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Event") && !eventEnCours)
+        {
+            state = EventState.ON_EVENT_ZONE;
+        }
+
+        /*if(collision.gameObject.layer == ZoneToGo && state == EventState.ON_EVENT)
+        {
+            
+        }*/
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Event") && !eventEnCours)
+        {
+            state = EventState.OUT_EVENT_ZONE;
+        }
+    }
 }
