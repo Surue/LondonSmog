6ac7604710b02ad66e2d9bd34b3bc6bc00209f5b
guillaume volgyesi <guillaumevolgyesi@gmail.com>
2017-12-02 15:53:31 +0100

    add camera script

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -4,14 +4,38 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    GameObject player;
+    private Vector3 offset;
+    float minPosX = -10.58f;
+    float maxPosX = 10.5f;
+    float minPosY = -1.7f;
+    float maxPosY = 28.6f;
 
 	void Start ()
     {
-		
+        player = GameObject.Find("Player");
+        offset = transform.position - player.transform.position; 
 	}
-	
-	void Update ()
+
+    void LateUpdate()
     {
-		
-	}
+        Vector3 CameraPosition = player.transform.position + offset;
+        if (CameraPosition.x < minPosX)
+        {
+            CameraPosition.x = minPosX;
+        }
+        if (CameraPosition.x > maxPosX)
+        {
+            CameraPosition.x = maxPosX;
+        }
+        if (CameraPosition.y < minPosY)
+        {
+            CameraPosition.y = minPosY;
+        }
+        if (CameraPosition.y > maxPosY)
+        {
+            CameraPosition.y = maxPosY;
+        }
+        transform.position = CameraPosition;
+    }
 }
