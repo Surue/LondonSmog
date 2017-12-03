7001e40bcb89c01a7ea82a9368f4e8599eb3b379
Surue <nicolas06062000@hotmail.com>
2017-12-03 12:36:18 +0100

    Add in project npc, complete menu Scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,8 +15,13 @@ public class GameManager : MonoBehaviour {
 		
 	}
 
-    void LoadSceneWithName(string nameScene)
+    public void LoadSceneWithName(string nameScene)
     {
         SceneManager.LoadScene(nameScene);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
